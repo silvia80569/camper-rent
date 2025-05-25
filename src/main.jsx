74ab2c29f3a65f6395d682ae35b4e45b602595a4
silvia80569 <157542289+silvia80569@users.noreply.github.com>
2025-05-25 20:44:14 +0200

    update deploy

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import { store } from "./redux/store.js";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/camper-rent/">
         <App />
       </BrowserRouter>
     </Provider>
